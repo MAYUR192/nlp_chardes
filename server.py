2e2d319cface4de072151bbfd6b08c7d83fadcf1
Mayur576 <mayur.joshi@digitalmain.com>
2022-03-14 02:00:58 +0530

    word2vec model

--- server.py
+++ server.py
@@ -3,12 +3,5 @@
 import sys
 import os
 
-""" Register BluePrint here"""
-
-os.environ['prod_env'] =  sys.argv[1]
-
-os.environ['url_param'] =  sys.argv[2]
-
-
 from app import app
 app.run(host='0.0.0.0', debug=True, port=5001, threaded=True)  # Start the server
